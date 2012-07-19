621b88e641aa8cc35be2c159d454f6b160b3e6f4
SERGIO ESTEBAN <rekiem.cs@gmail.com>
2012-07-19 17:23:40 -0400

    TEST RABO IT

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,6 +1,6 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Bienvenido to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 
 <?php /*
