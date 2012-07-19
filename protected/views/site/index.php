3b094742496eaaf0e8258b1fb4c1957e064d003e
SERGIO ESTEBAN <rekiem.cs@gmail.com>
2012-07-19 17:51:17 -0400

    ACTUALIZACION
    TABLAS MODELOS VISTAS

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,18 +2,3 @@
 
 <h1>Bienvenido to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-
-<?php /*
-<p>
-	<li><?php echo CHtml::link('Subitem',array('subitem/admin')); ?></li>
-	<li><?php echo CHtml::link('Item',array('item/admin')); ?></li>
-	<li><?php echo CHtml::link('Proyecto',array('proyecto/admin')); ?></li>
-	<li><?php echo CHtml::link('Centros de Costos',array('centrocosto/admin')); ?></li>
-	<li><?php echo CHtml::link('Tipos de Aportes',array('tipoaporte/admin')); ?></li>
-</p>
-*/?>
-<p>
-	<!-- <li><?php echo CHtml::link('Grafica',array('grafica/')); ?></li> -->
-
-</p>
-
