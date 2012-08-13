29aef3ba84991ac5da85771e30e0e4ce6b1f33a6
SERGIO ESTEBAN <rekiem.cs@gmail.com>
2012-08-13 15:39:45 -0400

    calendario

--- protected/views/aj/_crear.php
+++ protected/views/aj/_crear.php
@@ -25,12 +25,36 @@
 		</div><!-- row -->
 		<div class="row">
 		<?php echo $form->labelEx($model,'fechaCreacion'); ?>
-		<?php echo $form->textField($model, 'fechaCreacion'); ?>
+		<?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+			'model'=>$model,
+			'attribute'=>'fechaCreacion',
+
+			'options'=>array(
+				'showAnim'=>'fold',
+				'dateFormat'=>'yy-mm-dd',
+			),
+			'htmlOptions'=>array(
+				'style'=>'height:20px;',
+				'class'=>"hasDatepicker",
+			),
+		));?>
 		<?php echo $form->error($model,'fechaCreacion'); ?>
 		</div><!-- row -->
 		<div class="row">
 		<?php echo $form->labelEx($model,'fechaRespuesta'); ?>
-		<?php echo $form->textField($model, 'fechaRespuesta'); ?>
+		<?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+			'model'=>$model,
+			'attribute'=>'fechaRespuesta',
+
+			'options'=>array(
+				'showAnim'=>'fold',
+				'dateFormat'=>'yy-mm-dd',
+			),
+			'htmlOptions'=>array(
+				'style'=>'height:20px;',
+				'class'=>"hasDatepicker",
+			),
+		));?>
 		<?php echo $form->error($model,'fechaRespuesta'); ?>
 		</div><!-- row -->
 		<div class="row">
