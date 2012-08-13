fd8c45c3f244d398febd6ba4f49192f63dff51c2
SERGIO ESTEBAN <rekiem.cs@gmail.com>
2012-08-12 21:42:45 -0400

    Gracias tavique

--- protected/config/main.php
+++ protected/config/main.php
@@ -54,6 +54,8 @@ return array(
 		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
+			'urlSuffix'=>'.html',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
