7aaff03c596e926ac840d8131925f5ac53f2b351
Francisco Abalan <f.abalan@raboit.com>
2012-08-14 16:43:21 -0400

    Calendar controseg - parte1

---
+++ protected/views/abogadasecretaria/_crear.php
@@ -0,0 +1,76 @@
+<div class="form">
+
+
+<?php $form = $this->beginWidget('GxActiveForm', array(
+	'id' => 'abogadasecretaria-form',
+	'enableAjaxValidation' => false,
+));
+?>
+
+
+	<?php echo $form->errorSummary($model); ?>
+
+		<div class="row">
+		<?php echo $form->labelEx($model,'tipo'); ?>
+		<?php echo $form->textField($model, 'tipo', array('maxlength' => 45)); ?>
+		<?php echo $form->error($model,'tipo'); ?>
+		</div><!-- row -->
+		<div class="row">
+		<?php echo $form->labelEx($model,'estado'); ?>
+		<?php echo $form->textField($model, 'estado', array('maxlength' => 45)); ?>
+		<?php echo $form->error($model,'estado'); ?>
+		</div><!-- row -->
+		
+		<div class="row">
+		<?php echo $form->labelEx($model,'fechaCreacion'); ?>
+		<?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+			'model'=>$model,
+			'attribute'=>'fechaCreacion',
+			
+			// additional javascript options for the date picker plugin
+			'options'=>array(
+				'showAnim'=>'fold',
+				'dateFormat'=>'yy-mm-dd',
+			),
+			'htmlOptions'=>array(
+				'style'=>'height:20px;'
+			),
+		));?>
+		<?php echo $form->error($model,'fechaCreacion'); ?>
+		</div><!-- row -->
+		
+		<div class="row">
+		<?php echo $form->labelEx($model,'fechaRespuesta'); ?>
+		<?php $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+			'model'=>$model,
+			'attribute'=>'fechaRespuesta',
+			
+			// additional javascript options for the date picker plugin
+			'options'=>array(
+				'showAnim'=>'fold',
+				'dateFormat'=>'yy-mm-dd',
+			),
+			'htmlOptions'=>array(
+				'style'=>'height:20px;'
+			),
+		));?>
+		<?php echo $form->error($model,'fechaRespuesta'); ?>
+		</div><!-- row -->
+		
+		<div class="row">
+		<?php echo $form->labelEx($model,'observacion'); ?>
+		<?php echo $form->textField($model, 'observacion', array('maxlength' => 200)); ?>
+		<?php echo $form->error($model,'observacion'); ?>
+		</div><!-- row -->
+		<div class="row">
+		<?php echo $form->labelEx($model,'alerta'); ?>
+		<?php echo $form->textField($model, 'alerta'); ?>
+		<?php echo $form->error($model,'alerta'); ?>
+		</div><!-- row -->
+
+
+<?php
+echo GxHtml::submitButton(Yii::t('app', 'Guardar'));
+$this->endWidget();
+?>
+</div><!-- form -->
