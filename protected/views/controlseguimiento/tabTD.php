88f1575546d4c3cb2818102dc565849874731bb9
Francisco Abalan <f.abalan@raboit.com>
2012-08-14 12:45:40 -0400

    TABs

--- protected/views/controlseguimiento/tabTD.php
+++ protected/views/controlseguimiento/tabTD.php
@@ -3,19 +3,25 @@
 			'id'=> 'tabs_etapas',
 			'tabs' => array(
 				'Asesor juridico' => array('id' => 0,'content' => $this->renderPartial('//aj/ver_aj', array('model' => $model,),$this),),
-				'Director de proyecto' => array('id' => 1,'ajax' => $this->createUrl('/directorproyecto/create'),),
-				'Oficio conductor' => array('id' => 2,'content' => 'TEST',),
-				'Resolucion oficializacion' => array('id' => 3,'content' => 'TEST',),
-				'Secretaria' => array('id' => 4,'content' => 'TEST',),
-				'Contraloria' => array('id' => 5,'content' => 'TEST',),
-				'Publicacion' => array('id' => 6,'content' => 'TEST',),
-				'Proceso de evaluacion' => array('id' => 7,'content' => 'TEST',),
-				'Oficio conductor' => array('id' => 8,'content' => 'TEST',),
-				'Resolucion ADODES' => array('id' => 9,'content' => 'TEST',),
-				'Secretaria' => array('id' => 10,'content' => 'TEST',),
-				'Contraloria' => array('id' => 11,'content' => 'TEST',),
-				'Resolucion oficializada' => array('id' => 12,'content' => 'TEST',),
-				'Adjudicacion' => array('id' => 13,'content' => 'TEST',),
+				'Secretaria' => array( 'id' => 1,'content' => $this->renderPartial('//secretaria/ver_secretaria_resofl1', array('model' => $model->secretarias[0],),$this),),
+				'Contraloria' => array('id' => 2,'content' => $this->renderPartial('//contraloria/ver_contraloria', array('model' => $model->contralorias[0],),$this),),
+				'Publicacion' => array('id' => 3,'content' => 'TEST',),
+				
+				if($UTM>100){
+					'Recopilacion informacion' => array('id' => 4,'content' => 'TEST',),
+					'Abogada secretaria' => array('id' => 5,'content' => 'TEST',),
+					'Envio contrato firma' => array('id' => 6,'content' => 'TEST',),
+					'Resof contrato VAF' => array('id' => 7,'content' => 'TEST',),
+					'Resof contrato secr' => array('id' => 8,'content' => 'TEST',),
+					'Resof contrato cont' => array('id' => 9,'content' => 'TEST',),
+				}
+				
+				else{
+					'Resolucion oficializacion' => array('id' => 3,'content' => $this->renderPartial('//resolucionof/ver_resolucionof', array('model' => $model->resolucionof,),$this),),
+					'Resof contrato secr' => array('id' => 4,'content' => 'TEST',),
+					'Resof contrato cont' => array('id' => 5,'content' => 'TEST',),
+				}
+				
 			),
 				
 			'options' => array(
